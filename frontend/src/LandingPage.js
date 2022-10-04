d3ef557d3dac7b2a3fb4877e0057966719890ad5
Janvi08 <janviphadtare9@gmail.com>
2022-10-04 18:27:14 -0400

    Add frontend folder and Navbar

---
+++ frontend/src/LandingPage.js
@@ -0,0 +1,13 @@
+import './LandingPage.css';
+import NavBar from './Components/Navbar'
+
+
+function LandingPage() {
+  return (<div>
+    <NavBar/>
+  </div>
+    
+  );
+}
+
+export default LandingPage;
