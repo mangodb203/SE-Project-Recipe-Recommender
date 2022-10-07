0de2681c82ffbf7247027d8ff58f1078ab584117
MitanshuShaBa <42906426+MitanshuShaBa@users.noreply.github.com>
2022-10-07 11:04:13 -0400

    added forgot password flow

--- backend/controllers/auth/index.js
+++ backend/controllers/auth/index.js
@@ -1,6 +1,7 @@
 const User = require("../../models/User");
 const jwt = require("jsonwebtoken");
 const { isSignedIn } = require("./middlewares");
+const { transporter } = require("../../utils/transporter");
 
 exports.isSignedIn = isSignedIn;
 
@@ -45,6 +46,59 @@ exports.signOut = (req, res) => {
   res.json({
     message: "User signed out",
   });
+};
+
+exports.forgotPassword = (req, res) => {
+  const { email } = req.body;
+  User.findOne({ email }).exec((err, user) => {
+    if (err || !user) {
+      return res.status(400).json({
+        error: "Email does not exist",
+      });
+    }
+
+    const token = getToken(user);
+    const mailOptions = {
+      from: process.env.EMAIL,
+      to: email,
+      // to: "noreply.cookbook.ncsu@gmail.com",
+      subject: "Reset Password from CookBook",
+      html: `
+        <h2>Please click on the given link to reset your password</h2>
+        <p>${process.env.FRONTEND_URL}/reset-password/${token}</p>
+      `,
+    };
+
+    transporter.sendMail(mailOptions, (err, info) => {
+      if (err) {
+        return res.json(err);
+      }
+      res.status(200).send({ email, msg: "Email sent" });
+    });
+  });
+};
+
+exports.changePassword = (req, res) => {
+  const { token, password } = req.body;
+
+  try {
+    const { id, role, email } = jwt.verify(token, process.env.SECRET);
+
+    const { salt, encry_password } = new User({ password, email });
+
+    User.findOneAndUpdate({ email }, { salt, encry_password }).exec(
+      (err, user) => {
+        if (err || !user) {
+          return res.status(400).json({
+            error: "Email does not exist",
+          });
+        }
+        res.json({ msg: "Changed user password" });
+      }
+    );
+  } catch (e) {
+    return res.status(400).json(e);
+  }
 };
 
 const getToken = (user) => {
