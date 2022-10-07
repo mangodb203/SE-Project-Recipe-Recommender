90d668c2805cb5c893930f99151aa943a9bd120e
MitulPatel5522 <42907640+MitulPatel5522@users.noreply.github.com>
2022-10-07 12:06:30 -0400

    Moving LandingPage under components
    Created App to handle Routes

---
+++ frontend/src/Components/LandingPage/LandingPage.js
@@ -0,0 +1,14 @@
+import "./LandingPage.css";
+import NavBar from "../Navbar";
+import Home from "../Home";
+
+function LandingPage() {
+  return (
+    <div>
+      <NavBar />
+      <Home />
+    </div>
+  );
+}
+
+export default LandingPage;
