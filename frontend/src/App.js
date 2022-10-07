d9024509cc5e90dc05978316e0eaf26bcd493dd5
Janvi08 <janviphadtare9@gmail.com>
2022-10-07 15:17:42 -0400

    add css to cards

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,11 @@ import { Route, Routes, BrowserRouter } from "react-router-dom";
 import LandingPage from "./Components/LandingPage/LandingPage";
 import Recipes from "./Components/Recipes";
 import RecipeRecommendations from "./Components/RecipeRecommendations";
-
+import NavBar from "./Components/Navbar";
 function App() {
   return (
     <BrowserRouter>
+    <NavBar />
       <Routes>
         <Route path="/">
           <Route index element={<LandingPage />} />
