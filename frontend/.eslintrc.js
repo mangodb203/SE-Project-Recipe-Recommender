3423756b2f895335147863fa1d95a65431975360
Praneya Lal <praneya0028@gmail.com>
2022-10-09 19:51:29 -0400

    Eslint for Style and Syntax checking

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    no_console: 0,
+    no_empty: 0,
+    no_irregular_whitespace: 0,
+  },
+};
