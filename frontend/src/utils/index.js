8e94909d98d0106a8ec722d464a4a22b0a7e70fb
Shinigami <banerjeeindranil350@gmail.com>
2022-12-05 03:10:17 -0500

    Updated AWS EC2 endpoint

--- frontend/src/utils/index.js
+++ frontend/src/utils/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const server = axios.create({ baseURL: "http://ec2-3-238-186-29.compute-1.amazonaws.com:5000/" });
+const server = axios.create({ baseURL: "http://ec2-3-238-67-17.compute-1.amazonaws.com:5000/" });
 //const server = axios.create({ baseURL: "http://localhost:5000/" });
 
 export { server };
